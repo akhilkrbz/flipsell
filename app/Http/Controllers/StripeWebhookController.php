8a9f1196e9a004d69889800c5e19c70f8ef6f441
Akhil <akhilk1904@gmail.com>
2025-04-28 23:22:38 +0530

    Update

--- app/Http/Controllers/StripeWebhookController.php
+++ app/Http/Controllers/StripeWebhookController.php
@@ -35,62 +35,71 @@ class StripeWebhookController extends Controller
         }
 
         if ($event->type === 'payment_intent.succeeded') {
-            $intent = $event->data->object;
 
-            Log::channel('webhook')->info('Payment intent data');
-            Log::channel('webhook')->info($intent);
-            Log::channel('webhook')->info($intent->id);
-            Log::channel('webhook')->info($intent->amount);
+            try {
+                $intent = $event->data->object;
 
-            $payment_details = [
-                'payment_intent_id'     => $intent->id,
-                'amount_paid'           => $intent->amount / 100,
-                'currency'              => $intent->currency,
-                'payment_status'        => $intent->status,
-                'user_id'               => $intent->metadata->user_id ?? null,
-                'transaction_id'        => '',
-                'receipt_url'           => '',
-                'order_id'              => '',
-                'plan_id'               => $intent->metadata->plan_id ?? null,
-                'created_at'            => Carbon::now(),
-                'updated_at'            => Carbon::now(),
-            ];
+                Log::channel('webhook')->info('Payment intent data');
+                Log::channel('webhook')->info($intent);
+                Log::channel('webhook')->info($intent->id);
+                Log::channel('webhook')->info($intent->amount);
 
-            Log::channel('webhook')->info('Payment data');
-            Log::channel('webhook')->info(json_encode($payment_details));
-
-            // Save to database
-            $payment = Payment::create($payment_details);
-
-            Log::channel('webhook')->info('Payment data save');
-            Log::channel('webhook')->info($payment);
-
-            if($payment) {
-
-                Log::channel('webhook')->info('Payment data saved');
-                Log::channel('webhook')->info($payment);
-
-                $plan_data = Plan::where('id', $intent->metadata->plan_id)->first();
-
-                Log::channel('webhook')->info('plan data');
-                Log::channel('webhook')->info($plan_data);
-
-                $subscription_data = [
-                    'plan_id'       => $intent->metadata->plan_id,
-                    'user_id'       => $intent->metadata->user_id,
-                    'start_date'    => Carbon::now()->format('Y-m-d'),
-                    'end_date'      => Carbon::now()->addMonths($plan_data->month_no)->format('Y-m-d'),
-                    'payment_id'    => $payment->id
+                $payment_details = [
+                    'payment_intent_id'     => $intent->id,
+                    'amount_paid'           => $intent->amount / 100,
+                    'currency'              => $intent->currency,
+                    'status'                => $intent->status,
+                    'user_id'               => $intent->metadata->user_id ?? null,
+                    'transaction_id'        => '',
+                    'receipt_url'           => '',
+                    'order_id'              => '',
+                    'plan_id'               => $intent->metadata->plan_id ?? null,
+                    'created_at'            => Carbon::now(),
+                    'updated_at'            => Carbon::now(),
                 ];
 
-                Log::channel('webhook')->info('subscription data');
-                Log::channel('webhook')->info(json_encode($subscription_data));
+                Log::channel('webhook')->info('Payment data');
+                Log::channel('webhook')->info(json_encode($payment_details));
 
-                $saveSub = Subscription::create($subscription_data);
+                // Save to database
+                $payment = Payment::create($payment_details);
 
-                Log::channel('webhook')->info('subscription data save');
-                Log::channel('webhook')->info($saveSub);
+                Log::channel('webhook')->info('Payment data save');
+                Log::channel('webhook')->info($payment);
+
+                if($payment) {
+
+                    Log::channel('webhook')->info('Payment data saved');
+                    Log::channel('webhook')->info($payment);
+
+                    $plan_data = Plan::where('id', $intent->metadata->plan_id)->first();
+
+                    Log::channel('webhook')->info('plan data');
+                    Log::channel('webhook')->info($plan_data);
+
+                    $subscription_data = [
+                        'plan_id'       => $intent->metadata->plan_id,
+                        'user_id'       => $intent->metadata->user_id,
+                        'start_date'    => Carbon::now()->format('Y-m-d'),
+                        'end_date'      => Carbon::now()->addMonths($plan_data->month_no)->format('Y-m-d'),
+                        'payment_id'    => $payment->id
+                    ];
+
+                    Log::channel('webhook')->info('subscription data');
+                    Log::channel('webhook')->info(json_encode($subscription_data));
+
+                    $saveSub = Subscription::create($subscription_data);
+
+                    Log::channel('webhook')->info('subscription data save');
+                    Log::channel('webhook')->info($saveSub);
+                }
+            } catch (\Throwable $th) {
+                Log::channel('webhook')->info('Error');
+                Log::channel('webhook')->info($th->getMessage());
+                Log::channel('webhook')->info($th->getCode());
             }
+
+            
         }
         Log::channel('webhook')->info('Webhook ends');
         return response()->json(['status' => 'success']);
